5a0f9bfb978836b281ca9906527a74bab8975e63
Raliiinz <144474637+Raliiinz@users.noreply.github.com>
2024-05-25 13:45:03 +0300

    Add files via upload

--- WorkWithFileTest.java
+++ WorkWithFileTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class WorkWithFileTest {
 
     @Test
-    void testCheckEven() {
+    public void testCheckEven() {
         assertEquals(0, WorkWithFile.checkEven(new byte[0]));
         assertEquals(0, WorkWithFile.checkEven(new byte[]{2, 4, 6}));
         assertEquals(1, WorkWithFile.checkEven(new byte[]{1, 2, 3, 4}));
